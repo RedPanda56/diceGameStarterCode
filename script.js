8ac63dcc37b8693d20f51830f57db026debf0792
Garrett Michalke <garrett.michalke@gmail.com>
2017-02-22 23:47:18 +0000

    rollDice

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
   function rolldice(){
   //Place your code here 
 
-
-  };
+  }
   
+  $("#roll-dice").click(function(){
+    
+    
+    var dice = Math.random();
+    var dice1 = Math.floor(dice * 6);
+    
+    console.log(dice1);
+    
+    
+  });
 });
